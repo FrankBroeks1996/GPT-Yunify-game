79f2000a334cc898f15fd3069f147c81003c1d57
Frank <f.broeks@student.fontys.nl>
2018-05-31 13:47:47 +0200

    Added bullet interaction

--- Assets/BulletScript.cs
+++ Assets/BulletScript.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour {
 
+    public int Damage = 1;
     public float Speed = 10f;
     public float TimeAlive = 2f;
 	
 	// Update is called once per frame
 	void Update () {
         TimeAlive -= Time.deltaTime;
-        Debug.Log(TimeAlive);
         if(TimeAlive <= 0)
         {
             Destroy(gameObject);
@@ -18,4 +18,14 @@ public class BulletScript : MonoBehaviour {
 
         gameObject.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
 	}
+
+    void OnCollisionEnter(Collision objectHit)
+    {
+        Debug.Log("HIT!");
+        if (objectHit.gameObject.tag == "Enemy")
+        {
+            objectHit.gameObject.GetComponent<EnemyScript>().Health -= Damage;
+            Destroy(gameObject);
+        }
+    }
 }
