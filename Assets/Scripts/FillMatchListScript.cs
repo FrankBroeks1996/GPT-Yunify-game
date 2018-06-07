bd43b7d39be14c454c49eab7541e464542b1dcd2
Frank <f.broeks@student.fontys.nl>
2018-06-07 13:58:37 +0200

    Added start scene
    Not finished yet

---
+++ Assets/Scripts/FillMatchListScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking.Match;
+
+public class FillMatchListScript : MonoBehaviour {
+
+    public JoinButton ButtonPrefab;
+
+    public void AvailableMatchesList_OnAvailableMatchesChanged(List<MatchInfoSnapshot> matches)
+    {
+        ClearExistingButtons();
+        CreateNewJoinGameButtons(matches);
+    }
+
+    private void ClearExistingButtons()
+    {
+        var buttons = GetComponentsInChildren<JoinButton>();
+        foreach (var button in buttons)
+        {
+            Destroy(button.gameObject);
+        }
+    }
+
+    private void CreateNewJoinGameButtons(List<MatchInfoSnapshot> matches)
+    {
+        foreach (var match in matches)
+        {
+            var button = Instantiate(ButtonPrefab);
+            button.Initialize(match, transform);
+        }
+    }
+}
